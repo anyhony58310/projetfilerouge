c82248c8bccaf8ae5d7d8d6c9f38a421dac06457
anyhony58310 <anthony.regala58@gmail.com>
2024-10-08 16:35:25 +0200

    page d'accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 import Image from "next/image";
+import { PRODUCTS_CATEGORY_DATA } from "@arthur.eudeline/starbucks-tp-kit/data";
+import { BreadCrumbs } from "@arthur.eudeline/starbucks-tp-kit/components/breadcrumbs";
+import { ProductGridLayout } from "@arthur.eudeline/starbucks-tp-kit/components/products/product-grid-layout";
+import { ProductCardLayout } from "@arthur.eudeline/starbucks-tp-kit/components/products/product-card-layout";
+import { Button } from "@arthur.eudeline/starbucks-tp-kit/components/button";
+const categories = PRODUCTS_CATEGORY_DATA;
 
 export default function Home() {
+  console.log(categories);
   return (
-
+    <main>
+      {/* <BreadCrumbs> */}
+      
+      {/* </BreadCrumbs> */}
+      <ProductGridLayout products={categories[0].products}>
+        {(product) => (
+            <ProductCardLayout
+              product={product}
+              button={
+                <Button variant={"ghost"} fullWidth>
+                  Ajouter au panier
+                </Button>
+              }
+        />)}
+      </ProductGridLayout>
+    </main>
   );
 }
